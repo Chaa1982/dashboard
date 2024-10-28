032064e32f2e36c71d319049cdd9efd3129f4d5f
Andrii Chaplinskyi <chaa01051982@gmail.com>
2024-10-28 17:30:00 +0800

    added data about uaer in every post

--- src/components/posts/postItem/PostItem.tsx
+++ src/components/posts/postItem/PostItem.tsx
@@ -1,8 +1,10 @@
-import React, {FC} from "react";
-import {FlexColumn} from "../../shared/Flex";
+import React, {FC, useMemo} from "react";
 import {IDashboardPost} from "../types";
 import './postItem.css';
 import {ContentWrapper} from "../../shared/Common";
+import {useFetchData} from "../../../hooks/useFetchData";
+import {IDashboardUser} from "../../users/types";
+import {FlexRow} from "../../shared/Flex";
 
 
 interface IProps {
@@ -10,12 +12,20 @@ interface IProps {
 }
 
 export const PostItem: FC<IProps> = (props) => {
-    const {title, body} = props.post;
+    const options = useMemo(() => ({method: 'GET'}), []);
+    const {data, isLoading} = useFetchData<IDashboardUser>(`https://jsonplaceholder.typicode.com/users/${props.post.userId}`, options);
+
+    console.log('data>>>', data);
+
+
     return (
         <ContentWrapper flexDirection={'column'} alignItems={'flex-start'}>
-            <h3>{title.toUpperCase()}</h3>
-            <p>{body}</p>
+            <FlexRow alignItems={'center'} gap={'20px'}>
+                <h4>{data?.username}</h4>
+                <span>{data?.email}</span>
+            </FlexRow>
+            <h3>{props.post.title.toUpperCase()}</h3>
+            <p>{props.post.body}</p>
         </ContentWrapper>
-
     )
 }
